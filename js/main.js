cd825e10c18ce8c379ad9b476514ab4d68f6392e
shitcoinheiros <shitcoinheiros@gmail.com>
2026-01-29 00:00:41 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,12 +1,19 @@
-
 fetch("data/index.json")
   .then(r => r.json())
-  .then(artigos => {
+  .then(data => {
     const lista = document.getElementById("lista-artigos");
 
-    artigos.forEach(a => {
+    data.artigos.forEach(a => {
       const li = document.createElement("li");
-      li.innerHTML = `<a href="artigo.html?slug=${a.slug}">${a.titulo}</a>`;
+      li.innerHTML = `
+        <a href="artigo.html?slug=${a.slug}">
+          ${a.titulo}
+        </a>
+        <small> — ${a.resumo}</small>
+      `;
       lista.appendChild(li);
     });
+  })
+  .catch(err => {
+    console.error("Erro ao carregar artigos:", err);
   });
