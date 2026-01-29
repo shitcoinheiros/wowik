afce02e7e8f31fc27d9d6c7b2b02dc4e25e4f0db
shitcoinheiros <shitcoinheiros@gmail.com>
2026-01-28 23:39:43 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
 
+fetch("data/index.json")
+  .then(r => r.json())
+  .then(artigos => {
+    const lista = document.getElementById("lista-artigos");
+
+    artigos.forEach(a => {
+      const li = document.createElement("li");
+      li.innerHTML = `<a href="artigo.html?slug=${a.slug}">${a.titulo}</a>`;
+      lista.appendChild(li);
+    });
+  });
