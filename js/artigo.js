a74552d6f62bd8226a7f8a04c048cf6ca59d7e63
shitcoinheiros <shitcoinheiros@gmail.com>
2026-01-29 00:11:51 -0300

    Update artigo.js

--- js/artigo.js
+++ js/artigo.js
@@ -3,11 +3,21 @@ const slug = params.get("slug");
 
 fetch("data/index.json")
   .then(r => r.json())
-  .then(artigos => {
-    const artigo = artigos.find(a => a.slug === slug);
-    return fetch(artigo.arquivo);
+  .then(data => {
+    const artigo = data.artigos.find(a => a.slug === slug);
+
+    if (!artigo) {
+      document.getElementById("conteudo").innerHTML = "Artigo não encontrado";
+      return;
+    }
+
+    fetch(artigo.arquivo)
+      .then(r => r.text())
+      .then(md => {
+        document.getElementById("conteudo").innerHTML =
+          markdownToHtml(md);
+      });
   })
-  .then(r => r.text())
-  .then(md => {
-    document.getElementById("conteudo").innerHTML = markdownToHtml(md);
+  .catch(err => {
+    console.error("Erro ao carregar artigo:", err);
   });
